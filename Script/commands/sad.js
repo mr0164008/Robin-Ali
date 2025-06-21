802686719cf4c0c2a443cb9d85fe4ffb3ede186a
Robin-whoiam <168325047+Robin-whoiam@users.noreply.github.com>
2025-06-21 14:05:58 +0600

    Create sad.js

---
+++ Script/commands/sad.js
@@ -0,0 +1,29 @@
+/** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
+module.exports.config = {
+  name: "sad",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "CYBER-SUJON",
+  description: "VEDIO",
+  commandCategory: "Hình ảnh",
+  usages: "video",
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+   var hi = ["𝐒𝐭𝐚𝐭𝐮𝐬 𝐯𝐢𝐝𝐞𝐨"];
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link = [
+
+  "https://drive.google.com/uc?id=17Siy3m5zeLkokRoCyWwczk6zu526JUPF",];
+     var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/15.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/15.mp4"));    
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/15.mp4")).on("close",() => callback());
+   };
